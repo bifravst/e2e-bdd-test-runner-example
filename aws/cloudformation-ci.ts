cd00972787cfc445495a1af3c5f46122aec8072c
Markus Tacker <m@coderbyheart.com>
2019-06-22 23:48:48 +0200

    ci: use custom stack name

--- aws/cloudformation-ci.ts
+++ aws/cloudformation-ci.ts
@@ -16,6 +16,8 @@ import {
 } from '@aws-cdk/aws-codebuild';
 import { parse } from 'url';
 
+const STACK_ID = process.env.STACK_ID || 'bdd-feature-runner-aws-ci';
+
 /**
  * This is the CloudFormation stack sets up the continuous integration of the projec.
  */
@@ -101,7 +103,7 @@ class CIApp extends App {
         const Owner = repoUrl.path!.split('/')[1];
         const Repo = repoUrl.path!.split('/')[2];
 
-        new CI(this, 'bdd-feature-runner-aws-ci', {
+        new CI(this, STACK_ID, {
             Owner,
             Repo,
         });
